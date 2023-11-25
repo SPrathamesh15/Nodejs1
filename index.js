368342c6cdf6e68682bb30b7a21336435670a369
SPrathamesh15 <sprathamesh354@gmail.com>
2023-11-25 12:30:53 +0530

    created a fatArrow function and a student object

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log('Hello World')
+// console.log('Hello World')
+// fatArrow function to return product of two numbers
+// let mul = (num1, num2) => {
+//     return num1*num2
+// }
+let mul = (num1, num2) => num1 * num2;
+console.log(mul(4,2))
+// creating a student object
+let student = {
+    name: 'max',
+    age: 13,
+    roll_no: 13,
+    class: 8,
+    greet: function(){
+        console.log('Hi I am, ' + this.name)
+    }
+}
+student.greet()
