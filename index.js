410b383c8a86cc33f9b3096f36a021cb2db9f75e
SPrathamesh15 <sprathamesh354@gmail.com>
2023-11-25 13:13:32 +0530

    Arrays , spread and rest operators

--- index.js
+++ index.js
@@ -1,18 +1,11 @@
-// console.log('Hello World')
-// fatArrow function to return product of two numbers
-// let mul = (num1, num2) => {
-//     return num1*num2
-// }
-let mul = (num1, num2) => num1 * num2;
-console.log(mul(4,2))
-// creating a student object
-let student = {
-    name: 'max',
-    age: 13,
-    roll_no: 13,
-    class: 8,
-    greet: function(){
-        console.log('Hi I am, ' + this.name)
+let arr = ['apple', 'oranges' , ' ', 'mango', ' ' , 'lemon'];
+let newArr = arr.map(element => {
+    if (element === ' '){
+        return 'empty array'
+    } else {
+        return element
     }
-}
-student.greet()
+})
+console.log(arr)
+console.log(newArr)
+
