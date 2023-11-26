0b0a593781d6c061f855f91cd08d7a19a29c462a
SPrathamesh15 <sprathamesh354@gmail.com>
2023-11-26 12:26:27 +0530

    Writing and reading from a file

--- app.js
+++ app.js
@@ -1,14 +1,44 @@
-const http = require('http')
+const http = require('http');
+const fs = require('fs');
+
+const { parse } = require('path');
 const server = http.createServer((req, res) => {
     console.log('Hi, My name is Prathamesh')
-    const url = req.url
-    if (url === '/home'){
+    const url = req.url;
+    const method = req.method;
+    if (url === '/'){
+        const messageContent = fs.readFileSync('message.txt');
+        res.write('<html>')
+        res.write('<head><title>Enter Message</title></head>')
+        res.write(`<body><h1>${messageContent}</h1><form action="/message" method="POST"><input type="text" name="message"><button type="Submit">Send</button></input></form></body>`)
+        res.write('</html>')
+        return res.end()
+    }else if (url === '/home'){
         res.write('<html>')
         res.write('<head><title>Home Page</title></head>')
         res.write('<body><h1>Welcome home</h1></body>')
         res.write('</html>')
         return res.end()
-    } else if(url === '/about'){
+    } else if (url === '/message' && method === 'POST'){
+        const body = [];
+        //adding eventlistener to access data
+        req.on('data', (chunk)=>{
+            console.log(chunk);
+            body.push(chunk);
+        });
+        // adding another eventlistener to end the event to get the data
+        req.on('end', () => {
+            const parsedbody = Buffer.concat(body).toString();
+            const message = parsedbody;
+            const newMessage = parsedbody.split('=')[1]
+            console.log(message)
+            console.log(newMessage)
+            fs.writeFileSync('message.txt', newMessage)
+        })
+        res.statusCode = 302;
+        res.setHeader('Location', '/')
+        return res.end()
+    }else if(url === '/about'){
         res.write('<html>')
         res.write('<head><title>About Page</title></head>')
         res.write('<body><h1>Welcome to my About Page</h1></body>')
